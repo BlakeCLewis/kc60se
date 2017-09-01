81ac7868697c714a03cf10827ab64b499238d496
Blake C. Lewis <blake.lewis@gmail.com>
2017-09-01 12:40:46 -0500

    document capslock led pin

--- config.h
+++ config.h
@@ -37,10 +37,10 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #define DIODE_DIRECTION COL2ROW
 
-#define DEBOUNCING_DELAY 5
-
-#define BACKLIGHT_PIN   F5
-#define BACKLIGHT_LEVELS 6
+#define DEBOUNCING_DELAY  5
+#       CAPS_LOCK_PIN    B2  #defined in kc60se.h
+#define BACKLIGHT_PIN    F5
+#define BACKLIGHT_LEVELS  6
 
 // mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap
 #define LOCKING_SUPPORT_ENABLE
