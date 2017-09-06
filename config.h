7e2799094e1a1e078c86c4bf843a1a82035f6e99
Lewis, Blake C <lewisbc@umsystem.edu>
2017-09-06 12:47:53 -0500

    unused pins commented

--- config.h
+++ config.h
@@ -34,6 +34,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COL_PINS { F0, F1, E6, C7, C6, B6, D4, B1, B7, B5, B4, D7, D6, B3 }
 
 #define UNUSED_PINS
+                    //?  B0,E2,F4,F6,F7
 
 #define DIODE_DIRECTION COL2ROW
 
